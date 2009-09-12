a33733858640755e24a26fdc9f508a96bb4cdefb
Adrien Chauve <basbadri@gmail.com>
2009-09-12 18:29:19 +0200

    License changed to LGPL

--- tests/01_ex_basics.cpp
+++ tests/01_ex_basics.cpp
@@ -3,14 +3,12 @@
 This file is part of the LidarFormat project source files.
 
 LidarFormat is an open source library for efficiently handling 3D point
-clouds with a variable number of attributes at runtime. LidarFormat is
-distributed under the CeCILL-B licence. See Licence_CeCILL-B_V1-en.txt
-or http://www.cecill.info for more details.
+clouds with a variable number of attributes at runtime.
 
 
 Homepage:
 
-	https://fullanalyze.ign.fr/trac/LidarFormat
+	http://code.google.com/p/lidarformat
 
 Copyright:
 
@@ -20,33 +18,24 @@ Author:
 
 	Adrien Chauve
 
+Contributors:
+
+	Nicolas David, Olivier Tournaire
 
 
-This software is governed by the CeCILL-B license under French law and
-abiding by the rules of distribution of free software.  You can  use,
-modify and/ or redistribute the software under the terms of the CeCILL-B
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info".
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability.
+    LidarFormat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or
-data to be ensured and,  more generally, to use and operate it in the
-same conditions as regards security.
+    LidarFormat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL-B license and that you accept its terms.
+    You should have received a copy of the GNU Lesser General Public
+    License along with LidarFormat.  If not, see <http://www.gnu.org/licenses/>.
 
 ***********************************************************************/
 
