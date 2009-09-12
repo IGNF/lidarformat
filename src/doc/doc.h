14c80b4f6988f28bfa25412a2fa5eebe69568733
Adrien Chauve <Adrien Chauve@localhost>
2009-09-12 20:51:23 +0200

    Maj doc

--- src/doc/doc.h
+++ src/doc/doc.h
@@ -1,53 +1,29 @@
-/*! \mainpage LidarFormat Documentation
+/*! \mainpage LidarFormat developper documentation
  *
  *
  * \par
  *
  *
  * LidarFormat is an open source library for efficiently handling 3D point
- * clouds with a variable number of attributes at runtime. LidarFormat is
- * distributed under the CeCILL-B licence. See Licence_CeCILL-B_V1-en.txt
- * or http://www.cecill.info for more details.
+ * clouds with a variable number of attributes at runtime.
  *
  * \section intro_sec Introduction
  *
- * Homepage: https://fullanalyze.ign.fr/trac/LidarFormat
+ * Homepage: http://code.google.com/p/lidarformat/
  *
  * Copyright: Institut Geographique National & CEMAGREF (2009)
  *
  * Author: Adrien Chauve
  *
+ * Contributors: Nicolas David, Olivier Tournaire
+ *
+ * License: LGPL v3
+ *
  *
  *
  * \section install_sec Installation instructions
  *
- * \subsection dependencies Dependencies
- *
- * 		NB: On linux plateforms, you should use the official distribution packages.
- *
- *		Install the following tools or libraries:
- *			-CMake
- *			-Boost (>= 1.36)
- *			-Xerces
- *			-Code Synthesis XSD (can be downloaded at http://www.codesynthesis.com/download)
- *
- * \subsection compilation	Compilation:
- *
- *		On Linux platforms, enter in a terminal (in the LidarFormat/trunk directory):
- *
- *			\code
- *			ccmake .
- *			--> follow the CMake instructions, in particular:
- *			--> launch the configure step with 'c'
- *			--> set CMAKE_BUILD_TYPE to Release within CMake
- *			--> generate the configuration with 'g'
- *			make
- *			make test
- *			--> check all tests are passed!
- *			\endcode
- *
- *		** Make sure you compiled LidarFormat as a shared library, otherwise you will need
- *		to register the file formats in your code before using the library.
+ * See http://code.google.com/p/lidarformat/w/list
  *
  */
 
