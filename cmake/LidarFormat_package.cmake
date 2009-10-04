246bef539ade8772d1caf853b88cbb1b8e9611b1
Nicolas David <Nicolas David@localhost>
2009-10-04 21:09:49 +0200

    regroupement des fichiers cmake dans le dossier cmake et debut de diff?rentiation entre install et package

--- cmake/LidarFormat_package.cmake
+++ cmake/LidarFormat_package.cmake
@@ -1,44 +1,22 @@
 #################################################
-###           INSTALL 						  ###
+###           Package 						  ###
 #################################################
-#get_filename_component(LIDARFORMAT_CMAKE_SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-#IF(UNIX)
-#	SET(INSTALL_PREFIX "/usr/local" CACHE PATH " install path" )
-#ENDIF(UNIX)
-#IF(WIN32)
-#	SET(LidarFormat_INSTALL_DIR "LidarFormat")
-#	SET(INSTALL_PREFIX "C:/Program Files/MATIS" CACHE PATH " install path")
-#ENDIF(WIN32)
-#SET(CMAKE_INSTALL_PREFIX ${INSTALL_PREFIX} CACHE INTERNAL  " real install path" FORCE)
 
 IF(UNIX)
-    #SET(CMAKE_INSTALL_SO_NO_EXE "0")
 	INSTALL (FILES ${ALL_LIDAR_FORMAT_HEADER_FILES} DESTINATION include/LidarFormat COMPONENT headers)
 	INSTALL( FILES ${ALL_MODELS_HEADER_FILES} DESTINATION include/LidarFormat/models COMPONENT headers)
 	INSTALL( FILES ${ALL_GEOMETRY_HEADER_FILES} DESTINATION include/LidarFormat/geometry COMPONENT headers)
 	INSTALL( FILES ${ALL_TOOLS_HEADER_FILES} DESTINATION include/LidarFormat/tools COMPONENT headers)
 	INSTALL( FILES ${ALL_EXTERN_HEADER_FILES} DESTINATION include/LidarFormat/extern/matis COMPONENT headers)
-	#SET(RELATIVE_INCLUDE_PATH "../../include/LidarFormat")
-	#SET(RELATIVE_INCLUDE_EXTERN_PATH "../../include/extern")
-	#CONFIGURE_FILE(${LIDARFORMAT_CMAKE_SELF_DIR}/LidarFormatConfig.cmake.in ${CMAKE_CURRENT_SOURCE_DIR}/LidarFormatConfig.cmake @ONLY)
 	
 	INSTALL ( TARGETS LidarFormat DESTINATION lib COMPONENT library )
-			 
-	#INSTALL (	EXPORT LidarFormat-targets 
-	#			DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/LidarFormat
-	#			
-	#		)
-	#INSTALL (FILES LidarFormatConfig.cmake DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/LidarFormat)
 	
-	 set(CPACK_GENERATOR "DEB")
+	 SET(CPACK_GENERATOR "DEB")
 	 
 	 #dpkg-shlibdeps libLidarFormat.so
-	 set(CPACK_DEBIAN_PACKAGE_DEPENDS
+	 SET(CPACK_DEBIAN_PACKAGE_DEPENDS
 	         "libboost-filesystem1.37.0 (>= 1.37.0-1), libboost-system1.37.0 (>= 1.37.0-1), libc6 (>= 2.4), libgcc1 (>= 1:4.1.1), libstdc++6 (>= 4.2.1), libxerces-c28"
-	     )
-	     
-	 #set(DEBIAN_PACKAGE_BUILDS_DEPENDS "libboost-dev (>=1.36)")
+	     )	 
 	 
 ENDIF(UNIX)
 
@@ -52,13 +30,10 @@ IF(WIN32)
 	INSTALL( FILES ${ALL_GEOMETRY_HEADER_FILES} DESTINATION include/LidarFormat/geometry COMPONENT headers)
 	INSTALL( FILES ${ALL_TOOLS_HEADER_FILES} DESTINATION include/LidarFormat/tools COMPONENT headers)
 	INSTALL( FILES ${ALL_EXTERN_HEADER_FILES} DESTINATION include/LidarFormat/extern/matis COMPONENT headers)
-	#SET(RELATIVE_INCLUDE_PATH "../../include/LidarFormat")
-	#SET(RELATIVE_INCLUDE_EXTERN_PATH "../../include/extern")
-	#CONFIGURE_FILE(${LIDARFORMAT_CMAKE_SELF_DIR}/LidarFormatConfig.cmake.in ${CMAKE_CURRENT_SOURCE_DIR}/LidarFormatConfig.cmake @ONLY)
 	
 	INSTALL ( TARGETS LidarFormat DESTINATION lib COMPONENT library)	
 	
-	 #set(CPACK_GENERATOR "NSIS")
+	 SET(CPACK_GENERATOR "NSIS")
 
 ENDIF(WIN32)
 
