42d238ebfa99601f3d87ebcf4185c82507477639
Nicolas David <Nicolas David@localhost>
2010-11-13 12:11:12 +0100

    continue cleaning windows install path

--- cmake/LidarFormat_install.cmake
+++ cmake/LidarFormat_install.cmake
@@ -34,8 +34,8 @@ ENDIF(UNIX)
 
 
 IF(WIN32)
-	SET(RELATIVE_INCLUDE_PATH "../../include/LidarFormat")
-	SET(RELATIVE_INCLUDE_EXTERN_PATH "../../include/extern")
+	SET(RELATIVE_INCLUDE_PATH "../include/LidarFormat")
+	SET(RELATIVE_INCLUDE_EXTERN_PATH "../include/extern")
 	CONFIGURE_FILE(${LIDARFORMAT_CMAKE_SELF_DIR}/LidarFormatConfig.cmake.in ${CMAKE_CURRENT_SOURCE_DIR}/LidarFormatConfig.cmake @ONLY)
 	
 	INSTALL (FILES ${ALL_LIDAR_FORMAT_HEADER_FILES} DESTINATION include/LidarFormat COMPONENT headers)
@@ -44,9 +44,9 @@ IF(WIN32)
 	INSTALL( FILES ${ALL_TOOLS_HEADER_FILES} DESTINATION include/LidarFormat/tools COMPONENT headers)
 	INSTALL( FILES ${ALL_EXTERN_HEADER_FILES} DESTINATION include/LidarFormat/extern/matis COMPONENT headers)
 	
-	INSTALL ( TARGETS LidarFormat EXPORT LidarFormat-targets DESTINATION lib COMPONENT library)
+	INSTALL ( TARGETS LidarFormat EXPORT LidarFormat-targets DESTINATION ${CMAKE_INSTALL_PREFIX}/lib COMPONENT library)
 	INSTALL (EXPORT LidarFormat-targets 
-				DESTINATION lib/LidarFormat)
-	INSTALL (FILES LidarFormatConfig.cmake DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/LidarFormat)
+				DESTINATION ${CMAKE_INSTALL_PREFIX}/cmake)
+	INSTALL (FILES LidarFormatConfig.cmake DESTINATION ${CMAKE_INSTALL_PREFIX}/cmake)
 ENDIF(WIN32)
 
